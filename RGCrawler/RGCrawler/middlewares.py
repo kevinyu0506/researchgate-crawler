d8f433c391d4c768d5f3b9b5f2c03641b5811f00
Kevin Yu <kevinyu05062006@gmail.com>
2020-04-07 00:51:31 +0800

    Some refactoring

--- RGCrawler/RGCrawler/middlewares.py
+++ RGCrawler/RGCrawler/middlewares.py
@@ -11,13 +11,6 @@ from scrapy.utils.response import response_status_message
 from scrapy.http import TextResponse
 from scrapy.http import HtmlResponse
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-
 import time
 
 
@@ -116,29 +109,29 @@ class RgcrawlerDownloaderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class TooManyRequestsRetryMiddleware(RetryMiddleware):
-
-    def __init__(self, crawler):
-        super(TooManyRequestsRetryMiddleware, self).__init__(crawler.settings)
-        self.crawler = crawler
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        return cls(crawler)
-
-    def process_response(self, request, response, spider):
-        if request.meta.get('dont_retry', False):
-            return response
-        elif response.status == 429:
-            self.crawler.engine.pause()
-            time.sleep(5) # If the rate limit is renewed in a minute, put 60 seconds, and so on.
-            self.crawler.engine.unpause()
-            reason = response_status_message(response.status)
-            return self._retry(request, reason, spider) or response
-        elif response.status in self.retry_http_codes:
-            reason = response_status_message(response.status)
-            return self._retry(request, reason, spider) or response
-        return response
+# class TooManyRequestsRetryMiddleware(RetryMiddleware):
+#
+#     def __init__(self, crawler):
+#         super(TooManyRequestsRetryMiddleware, self).__init__(crawler.settings)
+#         self.crawler = crawler
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         return cls(crawler)
+#
+#     def process_response(self, request, response, spider):
+#         if request.meta.get('dont_retry', False):
+#             return response
+#         elif response.status == 429:
+#             self.crawler.engine.pause()
+#             time.sleep(5) # If the rate limit is renewed in a minute, put 60 seconds, and so on.
+#             self.crawler.engine.unpause()
+#             reason = response_status_message(response.status)
+#             return self._retry(request, reason, spider) or response
+#         elif response.status in self.retry_http_codes:
+#             reason = response_status_message(response.status)
+#             return self._retry(request, reason, spider) or response
+#         return response
 
 
 # class CustomProxyMiddleware(object):
@@ -158,45 +151,30 @@ class SeleniumChrome(object):
             return TextResponse(driver.current_url, body=body, encoding='utf-8', request=request)
         return None
 
-class SeleniumMiddleware():
-    # Middleware中会传递进来一个spider，这就是我们的spider对象，从中可以获取__init__时的chrome相关元素
-    LOAD_MORE_BTN = (By.XPATH,"//button[@class='nova-c-button nova-c-button--align-center nova-c-button--radius-m nova-c-button--size-xs nova-c-button--color-grey nova-c-button--theme-ghost nova-c-button--width-auto js-lite-click']")
-    REF_BTN = (By.XPATH, "//button[contains(@class,'nova-c-nav__item references')]")
 
+class SeleniumMiddleware(RgcrawlerDownloaderMiddleware):
+    # Middleware中会传递进来一个spider，这就是我们的spider对象，从中可以获取__init__时的chrome相关元素
+    '''
+    用chrome抓取页面
+    :param request: Request请求对象
+    :param spider: Spider对象
+    :return: HtmlResponse响应
+    '''
     def process_request(self, request, spider):
-        '''
-        用chrome抓取页面
-        :param request: Request请求对象
-        :param spider: Spider对象
-        :return: HtmlResponse响应
-        '''
         print(f"chrome is getting page")
         # 依靠meta中的标记，来决定是否需要使用selenium来爬取
         usedSelenium = request.meta.get('usedSelenium', False)
         if usedSelenium:
             try:
                 spider.driver.get(request.url)
-                time.sleep(15)
-
-                self.scroll_to_element(spider.driver, self.REF_BTN)
-                ref_btn = self.get_element_by(spider.driver, self.REF_BTN, 10)
-                ref_btn.click()
-                print(f"ref btn clicked")
-                time.sleep(3)
-                self.scroll_to_element(spider.driver, self.LOAD_MORE_BTN)
-                print(f"Scroll to load more btn")
-
-                while self.get_element_by(spider.driver, self.LOAD_MORE_BTN, 10) is not None:
-                    self.wait_for_clickable(spider.driver, self.LOAD_MORE_BTN)
-                    load_more_btn = self.get_element_by(spider.driver, self.LOAD_MORE_BTN)
-                    load_more_btn.click()
-                    print(f"load more btn clicked")
-                    time.sleep(1)
-                print(f"perform action done")
+                # print(f"Page loaded, start to sleep.")
+                # time.sleep(10)
+                # print(f"Sleep 10 sec.")
+                spider.start_interaction()
 
             except Exception as e:
                 print(f"chrome getting page error, Exception = {e}")
-                return HtmlResponse(url=request.url, status=500, request=request)
+                # return HtmlResponse(url=request.url, status=500, request=request)
             else:
                 print(f"return html response")
                 time.sleep(3)
@@ -207,30 +185,3 @@ class SeleniumMiddleware():
                                     # 最好根据网页的具体编码而定
                                     encoding='utf-8',
                                     status=200)
-
-    def get_element_by(self, driver, by, timeout=5):
-        try:
-            element = WebDriverWait(driver, timeout).until(
-                ec.visibility_of_element_located(by)
-            )
-        except (NoSuchElementException, TimeoutException) as err:
-            print(f"Exception Type: {type(err)}")
-            print(f"No such element: {(by,)}")
-            return None
-        return element
-
-    def scroll_to_element(self, driver, by):
-        element = self.get_element_by(driver, by)
-
-        actions = ActionChains(driver)
-        actions.move_to_element(element).perform()
-
-    def wait_for_clickable(self, driver, locator, timeout=10):
-        """ Check the element if it is clickable """
-        try:
-            WebDriverWait(driver, timeout).until(
-                ec.element_to_be_clickable(locator)
-            )
-        except (NoSuchElementException, TimeoutException) as err:
-            return False
-        return True
--- RGCrawler/RGCrawler/spiders/rg_spider.py
+++ RGCrawler/RGCrawler/spiders/rg_spider.py
@@ -5,12 +5,13 @@ from RGCrawler.items import PaperItem
 
 from scrapy import Request
 
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class RGSpider(scrapy.Spider):
@@ -35,13 +36,16 @@ class RGSpider(scrapy.Spider):
     REFERENCE_DATE = ".//div[@class='nova-v-publication-item__meta-right']/ul/li[@class='nova-e-list__item nova-v-publication-item__meta-data-item']/span/text()"
     REFERENCE_CONFERENCE = ".//div[@class='nova-v-publication-item__meta-right']/ul//a[@class='nova-e-link nova-e-link--color-inherit nova-e-link--theme-bare']/text()"
     TITLE = "//h1/text()"
-    SITE_URL = "https://www.researchgate.net/publication/322584236_Towards_the_Understanding_of_Gaming_Audiences_by_Modeling_Twitch_Emotes"
+    # SITE_URL = "https://www.researchgate.net/publication/322584236_Towards_the_Understanding_of_Gaming_Audiences_by_Modeling_Twitch_Emotes"
     # SITE_URL = "https://www.researchgate.net/publication/314361240_Spice_up_Your_Chat_The_Intentions_and_Sentiment_Effects_of_Using_Emoji"
-    LOAD_MORE_REQUEST = "https://www.researchgate.net/lite.PublicationDetailsLoadMore.getReferencesByOffset.html?publicationUid=322584236&offset=10"
-    LOAD_MORE_BTN = (By.XPATH, "//button[@class='nova-c-button nova-c-button--align-center nova-c-button--radius-m nova-c-button--size-xs nova-c-button--color-grey nova-c-button--theme-ghost nova-c-button--width-auto js-lite-click']")
-    LOAD_MORE_BTN_XPATH = "//button[@class='nova-c-button nova-c-button--align-center nova-c-button--radius-m nova-c-button--size-xs nova-c-button--color-grey nova-c-button--theme-ghost nova-c-button--width-auto js-lite-click']"
+    SITE_URL = "https://www.researchgate.net/publication/313910429_Are_Emojis_Predictable"
+    LOAD_MORE_BTN = (By.XPATH, "//span[contains(text(), 'Show more')]/..")
     REF_BTN = (By.XPATH, "//button[contains(@class,'nova-c-nav__item references')]")
-    REF_BTN_XPATH = "//button[contains(@class,'nova-c-nav__item references')]"
+
+    POPUP_BTN = (By.XPATH, "//button[@class='nova-c-button nova-c-button--align-center nova-c-button--radius-m nova-c-button--size-xs nova-c-button--color-grey nova-c-button--theme-bare nova-c-button--width-auto']")
+
+    CITATION_BTN = (By.XPATH, "//div[@class='nova-o-pack__item']//div[contains(text(),'Citations')]")
+    REFERENCE_BTN = (By.XPATH, "//div[@class='nova-o-pack__item']//div[contains(text(),'References')]")
 
     start_urls = [SITE_URL]
 
@@ -53,23 +57,10 @@ class RGSpider(scrapy.Spider):
         super(RGSpider, self).__init__()
 
     def start_requests(self):
-        # overriding start_requests() is not usually necessary unless:
-        # - we need to use selenium to interact with a form
-        # - we want to customize parts of the request object
+        # since we need selenium to interact with js btn, we're going to override
+        # the request generated automatically by the framework and create our own.
         #
-        # if we don't override start_requests(),
-        # scrapy generates requests from urls in start_urls[]
-        # and calls parse() as the default callback
-        #
-        # for s in self.start_urls:
-        #     request = self.make_requests_from_url(s)
-        #     # request.callback = self.parse_shell
-        #
-        #     yield request
-        #
-        # since we need selenium for the js form interactions we're going to circumvent creating a request
-        # instead we create a scrapy response object from the selenium form results
-        # then we use the xpath selector of the response object to create links to follow
+        # We yield this request and pass it to our custom SeleniumMiddleware for further handling
 
         yield Request(
             url=self.SITE_URL,
@@ -77,41 +68,31 @@ class RGSpider(scrapy.Spider):
             callback=self.parse_reference
         )
 
-    # def get_selenium_response(self):
-    #     self.logger.info("Start to get selenium response")
-    #     self.driver.get(self.SITE_URL)
-    #     self.scroll_to_element(self.driver, self.REF_BTN)
-    #     self.logger.info("Scroll to ref tab.")
-    #     ref_btn = self.get_element_by(self.driver, self.REF_BTN, 10)
-    #     ref_btn.click()
-    #     self.logger.info("Ref tab clicked.")
-    #     sleep(3)
-    #     while self.get_element_by(self.driver, self.LOAD_MORE_BTN, 5) is not None:
-    #         load_more_btn = self.get_element_by(self.driver, self.LOAD_MORE_BTN)
-    #         load_more_btn.click()
-    #         self.logger.info("Load more btn clicked.")
-    #         sleep(1)
-    #
-    #     self.logger.info("Load all btn.")
-    #     sleep(3)
-    #
-    #     # r = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8')
-    #     r = self.driver.page_source
-    #     self.logger.info("Generate response")
-    #     # self.parse_reference(response)
-    #     return r
+    def start_interaction(self):
+        self.logger.info("Start interaction.")
+
+        ref_btn = self.get_element_by(self.driver, self.REFERENCE_BTN)
+        ref_btn.click()
+        self.logger.info("Sleep 15 sec.")
+        while self.get_element_by(self.driver, self.LOAD_MORE_BTN, 10) is not None:
+            # self.logger.info("Load more tab fetched.")
+            # load_more_btn = self.get_element_by(self.driver, self.LOAD_MORE_BTN)
+            # load_more_btn.click()
+            self.driver.execute_script("document.getElementsByClassName('nova-c-button nova-c-button--align-center nova-c-button--radius-m nova-c-button--size-xs nova-c-button--color-grey nova-c-button--theme-ghost nova-c-button--width-auto js-lite-click')[0].click();")
+            self.logger.info("Load more btn clicked.")
+            sleep(1)
+
+        self.logger.info("Load all btn, interaction complete.")
+        sleep(3)
 
     def parse_reference(self, response):
         self.logger.info("Start parsing")
-
-        # response = self.get_selenium_response()
 
         p_item = PaperItem()
         p_item['root_title'] = response.xpath(self.TITLE).get()
         yield p_item
 
         references = response.xpath(self.REFERENCES)
-
         for index, reference in enumerate(references):
             rf_item = ReferenceItem()
             rf_item['id'] = index
@@ -120,11 +101,12 @@ class RGSpider(scrapy.Spider):
                 rf_item['link'] = "https://www.researchgate.net/" + reference.xpath(self.REFERENCE_LINK).get()
             elif reference.xpath(self.REFERENCE_TITLE_UNLINKABLE).get():
                 rf_item['title'] = reference.xpath(self.REFERENCE_TITLE_UNLINKABLE).get()
+                rf_item['link'] = None
             rf_item['date'] = reference.xpath(self.REFERENCE_DATE).get()
             rf_item['conference'] = reference.xpath(self.REFERENCE_CONFERENCE).get()
             yield rf_item
 
-        self.logger.info("Parse selenium response succeed.")
+        self.logger.info("Parse complete.")
         self.driver.quit()
 
     def get_element_by(self, driver, by, timeout=5):
@@ -138,8 +120,19 @@ class RGSpider(scrapy.Spider):
             return None
         return element
 
-    def scroll_to_element(self, driver, by):
-        element = self.get_element_by(self.driver, by)
+    # def wait_clickable(self, driver, by, timeout=10):
+    #     try:
+    #         element = WebDriverWait(driver, timeout).until(
+    #             ec.element_to_be_clickable(by)
+    #         )
+    #     except (NoSuchElementException, TimeoutException) as err:
+    #         self.logger.error(f"Exception Type: {type(err)}")
+    #         self.logger.error(f"No such element: {(by, )}")
+    #         return False
+    #     return True
 
-        actions = ActionChains(driver)
-        actions.move_to_element(element).perform()
+    # def scroll_to_element(self, driver, by):
+    #     element = self.get_element_by(self.driver, by)
+    #
+    #     actions = ActionChains(driver)
+    #     actions.move_to_element(element).perform()
