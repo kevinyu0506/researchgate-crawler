938bdef7a5cd63f3a477486b6e787b0894771cdc
kevinyu0506 <kevinyu05062006@gmail.com>
2020-12-04 00:44:46 +0800

    Complete parsing json & refresh request cookies

--- src/spider.py
+++ src/spider.py
@@ -11,13 +11,13 @@ import path as path
 
 class PaperSpider(scrapy.Spider):
     name = 'paperspider'
-    #start_urls = ['https://www.researchgate.net/publication/338506484_Less_Is_More_Learning_Highlight_Detection_From_Video_Duration']
-    start_urls = ['https://www.researchgate.net/publication/323165042_Attention-based_Deep_Multiple_Instance_Learning']
+    start_urls = ['https://www.researchgate.net/publication/338506484_Less_Is_More_Learning_Highlight_Detection_From_Video_Duration']
+    #start_urls = ['https://www.researchgate.net/publication/323165042_Attention-based_Deep_Multiple_Instance_Learning']
 
     custom_settings = {
         'LOG_LEVEL': 'INFO',
         'DEPTH_LIMIT': 1,
-        'DOWNLOAD_DELAY': 2.5,
+        'DOWNLOAD_DELAY': 5,
     }
 
     def parse(self, response):
@@ -35,8 +35,8 @@ class PaperSpider(scrapy.Spider):
         reference_count = response.xpath(path.REFERENCES_COUNT).get() if not None else ""
         reference_count = reference_count[reference_count.find("(")+1:reference_count.find(")")]
 
-        file_name = title.replace(" ","_")
-        target_file = open(f'../output/{file_name}.json', 'w')
+        self.file_name = title.replace(" ","_")
+        target_file = open(f'../output/{self.file_name}.json', 'w')
 
         paper_info = {
            'title': title,
@@ -54,7 +54,7 @@ class PaperSpider(scrapy.Spider):
 
         while get_reference(uid=publication_id, offset=offset).status_code == 200:
             ref_response = get_reference(uid=publication_id, offset=offset)
-            #self.logger.info(ref_response.status_code)
+            self.logger.info(ref_response.status_code)
             if (ref_response.text == ''):
                 break
             html = HTML(html=ref_response.text)
@@ -62,17 +62,11 @@ class PaperSpider(scrapy.Spider):
             if len(links) == 0:
                 break
             for link in links:
-                #self.logger.info(path.BASE_URL+link)
                 follow_urls.add(path.BASE_URL+link)
             offset = offset + 5
 
         for reference in response.xpath(path.REFERENCES):
-            reference_title = reference.xpath(path.REFERENCES_TITLE).get() if reference.xpath(path.REFERENCES_TITLE).get() is not None else ""
             reference_link = path.BASE_URL+reference.xpath(path.REFERENCES_LINK).get() if reference.xpath(path.REFERENCES_LINK).get() is not None else ""
-            reference_type = reference.xpath(path.REFERENCES_TYPE).get() if reference.xpath(path.REFERENCES_TYPE).get() is not None else ""
-            reference_date = reference.xpath(path.REFERENCES_DATE).get() if reference.xpath(path.REFERENCES_DATE).get() is not None else ""
-
-            #self.logger.info(reference_link)
             if (reference_link != ''):
                 follow_urls.add(reference_link)
 
@@ -80,16 +74,16 @@ class PaperSpider(scrapy.Spider):
 
         for url in follow_urls:
             if url is not None:
-                yield response.follow(url, self.reference_parse, cb_kwargs=dict(file_name=file_name))
+                yield response.follow(url, self.reference_parse)
 
-    def reference_parse(self, response, file_name):
+    def reference_parse(self, response):
         reference_title = response.xpath(path.TITLE).get() if not None else ""
         reference_conference = response.xpath(path.CONFERENCE).get() if not None else ""
         reference_doi = response.xpath(path.DOI).get() if not None else ""
         reference_citation_count = response.xpath(path.CITATIONS_COUNT).get() if not None else ""
         reference_citation_count = reference_citation_count[reference_citation_count.find("(")+1:reference_citation_count.find(")")]
 
-        target_file = open(f'../output/{file_name}.json', 'a')
+        target_file = open(f'../output/{self.file_name}.json', 'a')
 
         ref_info = {
             'reference title': reference_title,
@@ -99,23 +93,19 @@ class PaperSpider(scrapy.Spider):
         }
 
         target_file.write(json.dumps(ref_info, indent=4)+',\n')
-
         target_file.close()
 
     def closed(self, reason):
-        with open(f'../output/Attention-based_Deep_Multiple_Instance_Learning.json', 'rb+') as target_file:
+        with open(f'../output/{self.file_name}.json', 'rb+') as target_file:
             target_file.seek(-1, os.SEEK_END)
             target_file.truncate()
             target_file.seek(-1, os.SEEK_END)
             target_file.truncate()
-
             target_file.close()
 
-
-        target_file = open(f'../output/Attention-based_Deep_Multiple_Instance_Learning.json', 'a')
-
+        target_file = open(f'../output/{self.file_name}.json', 'a')
         target_file.write(']}')
         target_file.close()
 
-        data = parse('../output/Attention-based_Deep_Multiple_Instance_Learning.json')
-        self.logger.info(f'final scrap data length: {data}')
+        data = parse(f'../output/{self.file_name}.json')
+        self.logger.info(f'final scrap data length: {len(data["result"])}')
--- src/util/helper.py
+++ src/util/helper.py
@@ -6,14 +6,15 @@ def parse(fname):
     with open(fname) as f:
         data = json.load(f)
 
-        return len(data)
+        return data
 
 def get_reference(uid='319879823', offset='5'):
+
     headers = {
         'authority': 'www.researchgate.net',
         'content-length': '0',
         'accept': 'text/html',
-        'rg-request-token': 'aad-r2ot7153OSBz4sxXyzKLnWJK0iXVS3/h+YnCcFs22ouAxpM1/qqdnuoQnJVjuMd3kclPKhZ6EdZ7wSVOQNxTM9KPlcDnJHXf9DSgspXnpXfkOLTtMnaRei6dA85lYXL9XryhQTTujM30J5QFbujeaa89MtwlGKIFUjePGg1IXJSk/FnJ2pZBwUPqEpUmXV/4Li6wKjOhJFN0aQelG4dXXwyZZrFzmhFgIDTCUfTl5frJFM3YUnrwUE6y6VBcBdEOZ1KXv6DJTIq1IQQXxzg=',
+        'rg-request-token': 'aad-PO8We2XyFEq7IY19B8oNNj/a4aUshIutUUGC3mgWq3BsdUQF8tfs96B9TTIoTXGwS9SfebfU7Dkg7fQmQpQTWPhzI3EYSCs9faylpMDkIBxBH1VMiAbbxjH+ftydHhmr9LamkHlNnaoQXgTNZ8lX5PJ8bySqhtRxDSL/9dIaEfxKMCmc99YQO+IROujHo2c4COyuSZpwXH1na4FfryDwgzxRu6hst24Wevuzn+UygilS9acjWlDQtkqVhrkNLU1bEbSVEmECfkIXJCJZYxLgeViYIL+f18FCtC4=',
         'x-requested-with': 'XMLHttpRequest',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
         'content-type': 'application/json',
@@ -23,7 +24,7 @@ def get_reference(uid='319879823', offset='5'):
         'sec-fetch-dest': 'empty',
         'referer': 'https://www.researchgate.net/publication/319879823_Quantification_of_histochemical_staining_by_color_deconvolution',
         'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
-        'cookie': 'did=vjRyRJZbedvZQLFCKBuv1YpgBR5S8Q9hJQly7N7u9o4S8HAbkBjtde3EgxAhx057; ptc=RG1.6749038922759709122.1600151195; _ga=GA1.2.584583719.1600151196; rghfp=true; __cfduid=d133d4635e1ddc3dacb85215b40c27f651606742182; _gid=GA1.2.1981096394.1606742186; __gads=ID=1c27639bd5e7d898:T=1600151214:R:S=ALNI_MZsL7p_odQKVFc_23pjdDf-qMY3LQ; _pbjs_userid_consent_data=3524755945110770; SKpbjs-unifiedid=%7B%22TDID%22%3A%220bebe803-2b3a-4ff6-bb71-cb39ca6c792e%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222020-10-30T13%3A16%3A35%22%7D; SKpbjs-unifiedid_last=Mon%2C%2030%20Nov%202020%2013%3A16%3A35%20GMT; SKpbjs-id5id=%7B%22created_at%22%3A%222020-11-30T16%3A17%3A26.695Z%22%2C%22id5_consent%22%3Atrue%2C%22original_uid%22%3A%22ID5-ZHMOUu-1Mf44ga9Pt-tvEjmX2pzEIdtgzv3Q0VUJ7w%22%2C%22universal_uid%22%3A%22ID5-ZHMONd98_wBogEhlmNHSs6N5n-BA0B7os-4P8cQugw%22%2C%22signature%22%3A%22ID5_AeV0LFxVhIjjzWseyarDrWHYlE1lHVcxS3_aNQG92rFrlPh5P7h1Pl5fbP3zliDzfu_0k9e9126v0bBLQMaJ0mc%22%2C%22link_type%22%3A2%2C%22cascade_needed%22%3Atrue%7D; SKpbjs-id5id_last=Mon%2C%2030%20Nov%202020%2016%3A17%3A27%20GMT; id5id.1st_212_nb=2; GED_PLAYLIST_ACTIVITY=W3sidSI6IlNnM1AiLCJ0c2wiOjE2MDY3NTYzMTEsIm52IjoxLCJ1cHQiOjE2MDY3NDIxOTAsImx0IjoxNjA2NzU2MzExfV0.; chseen=1; ciiir=1; cf_clearance=2583ad47c568d2a037902e1d5f6c877aa9720d1a-1606757132-0-250; sid=p2Er7uoLmLdYg9FzzMPQLSoSuK7f1yFWRIxdtQ6i0NVnIwwKX0nQdsR89M6qKmT01j7ihNQ0jiQyXmG7PGm0FHDUZchJmNko7KtINpBqU8DH4PABkSZd057XH4fToa1a; captui=ZjQ2ZWQ2ZjEwZmNjZDU2OWQ2ZGI4ZWNkYTMzN2FiM2QwNjdlYzZmZTkxYWJmYzE0ZDM2YzlmMTk4MjI3NTU2MF9LMVFMeVBXZEdodXU1NHJPaXdoZnZBcnZDOFJRUVFZb05Jelk%3D; _gat_UA-58591210-1=1',
+        'cookie': 'd=d7e37eb6fe4b8bf0b4765fd1f4e8ae1b81605585347; did=Dreekc0WDMYXrAbcNViNVr8UCjuW7Fg3wdx3cynOByamJR1X2JC6lX8dtp70LySI; ptc=RG1.8911876454667923880.1605585347; _ga=GA1.2.1280472156.1605585351; _pbjs_userid_consent_data=3524755945110770; _pubcid=e2bce89e-159a-4627-93f5-5b9226668778; cto_bidid=n9UaLF9ZQVNDOSUyQkpuRHd0SEpoU0ZxdHdFT3pUOSUyQjlBV2dRTWRibEVNJTJGTXRRbVg0dVlRdEZKOGRPS2VpTnE2dm5SUGRVYm9uVHNNNlpXJTJCRDRnbVRjJTJCZW9oOFElM0QlM0Q; cto_bundle=uH-C9l9iQjhOJTJGTk42MjFzcm5XRnByJTJGajNjcU5FcWd5WldwTE1EZ0RBJTJGVFNpbkNmV2ZDYThwVndkY3RUakluRjFidktxSWJTeE5RUmhMY1UlMkJhQ2xRWVNEVyUyRlczeUI0OWpKUXV0SE1JOWdvZTEyM3I5MnFIdUhkbm0yNG9YdVVMZUhUdlo; pbjs-unifiedid=%7B%22TDID%22%3A%22a86f2d96-5d6d-448d-90f0-c23f628cc43c%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222020-10-17T03%3A55%3A53%22%7D; __gads=ID=ee7b2c56431a61df:T=1605585353:S=ALNI_MYyXIowNaw9sKyTv8fDlUicAosoOQ; sid=oUXQ7Sr3KUs9pl3gRgCJscOlYeErMeiUzIJG1CSOoB248iWk80j4IXwjL0Li8gAvtmKtNB2nV3WJCftwBFWItujWFQ2Q6TlmY8q1m02gCs3D7w3uyROifU3Q88MyW8kG; _gid=GA1.2.3380060.1607004342; SKpbjs-unifiedid=%7B%22TDID%22%3A%22a86f2d96-5d6d-448d-90f0-c23f628cc43c%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222020-11-03T14%3A20%3A49%22%7D; SKpbjs-unifiedid_last=Thu%2C%2003%20Dec%202020%2014%3A20%3A50%20GMT; id5id.1st_212_nb=0; SKpbjs-id5id=%7B%22created_at%22%3A%222020-12-03T14%3A20%3A50.775Z%22%2C%22id5_consent%22%3Atrue%2C%22original_uid%22%3A%22ID5-ZHMOWi06AeEFhKB_CGlGuv-Z67tiyoT9hy34V4mJTg%22%2C%22universal_uid%22%3A%22ID5-ZHMOfjjnwa3ehPlkEY7LcnToCd0zgSDLm9T86AFaag%22%2C%22signature%22%3A%22ID5_AYcwZX15AowM6jIW-0TaeaXGPF5CX_MSbalOTHthh_qLzpFyC4-qGAtE1O88XkZq2QtNEbOnM9Y_yM9TSg9XwLs%22%2C%22link_type%22%3A2%2C%22cascade_needed%22%3Atrue%7D; SKpbjs-id5id_last=Thu%2C%2003%20Dec%202020%2014%3A20%3A50%20GMT; rghfp=true; _gat_UA-58591210-1=1; GED_PLAYLIST_ACTIVITY=W3sidSI6IklwNTciLCJ0c2wiOjE2MDcwMTIzOTIsIm52IjowLCJ1cHQiOjE2MDcwMDQzNDUsImx0IjoxNjA3MDEyMzg3fV0.; _gat=1',
     }
 
     params = (
